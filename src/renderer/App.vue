260f64f25f403c486b62e189cbae8e066660c68c
fifthThirteen <570205219@qq.com>
2019-02-22 15:09:22 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'stpc1901'
+  }
+</script>
+
+<style lang="scss">
+  /* CSS */
+</style>
